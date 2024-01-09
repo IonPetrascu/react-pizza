adc1c7464c935cc6d9725ebc32d0dcd553b2c7be
Ion Petrascu <ionpetrascuc0318@gmal.com>
2024-01-09 15:32:59 +0200

    made receiving pizzas from the backend using axios request and useEffect

--- src/App.js
+++ src/App.js
@@ -3,22 +3,31 @@ import Header from './Components/Header';
 import Categories from './Components/Categories';
 import Sort from './Components/Sort';
 import PizzaBlock from './Components/PizzaBlock';
-import pizzas from './assets/pizzas.json';
 
-console.log(pizzas);
+import axios from 'axios';
+import React, { useEffect } from 'react';
+
 function App() {
+  const [items, setItems] = React.useState([]);
+
+  useEffect(() => {
+    axios.get('https://659d3c46633f9aee7908fa23.mockapi.io/items')
+      .then((res) => setItems(res.data));
+  }, []);
+
+  
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
           <div className="content__top">
-            {/* <Categories /> */}
+            <Categories />
             <Sort />
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((pizza) => (
+            {items.map((pizza) => (
               <PizzaBlock key={pizza.id} {...pizza} />
             ))}
           </div>
